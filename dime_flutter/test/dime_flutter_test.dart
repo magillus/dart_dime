4279da2f6c87004ced2bfa5d7e7cceea217635e5
Mateusz Perlak <mati.akip@gmail.com>
2022-03-19 16:16:28 -0500

    Added unit tests for dime scopes

--- dime_flutter/test/dime_flutter_test.dart
+++ dime_flutter/test/dime_flutter_test.dart
@@ -1,5 +1,46 @@
+import 'package:dime_flutter/dime_flutter.dart';
+import 'package:dime_flutter/flutter_scope.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const title = "TITLE";
+
 void main() {
-  test('adds one to input values', () {});
+  testWidgets("Test scope widget", (widgetTester) async {
+    dimeInstall(StringModule());
+    final testWidget = Directionality(
+      textDirection: TextDirection.ltr,
+      child: DimeScopeFlutter(
+        child: TitleView(),
+        modules: [StringScopedModule()],
+      ),
+    );
+
+    await widgetTester.pumpWidget(testWidget);
+
+    await widgetTester.ensureVisible(find.text("Scoped title"));
+  });
+}
+
+class StringModule extends BaseDimeModule {
+  @override
+  void updateInjections() {
+    addSingle<String>("Main title");
+  }
+}
+
+class StringScopedModule extends BaseDimeModule {
+  @override
+  void updateInjections() {
+    addSingle<String>("Scoped title");
+  }
+}
+
+class TitleView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final scope = DimeFlutter.scopeOf(context);
+    final titleText = scope.get();
+    return Text(titleText);
+  }
 }
